8669f8fe51749abacc2125fc1cc3ef53e0421340
Claus Spacil <claus@spacil.com>
2021-03-06 14:43:06 +0100

    fixed version numbers

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/flexsurfer/rn-shadow-steroid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/clojurescript "1.10.741"]
-                 [reagent "0.10.2"]
+                 [reagent "1.0.0"]
                  [re-frame "1.2.0"]
                  [re-frame-steroid "0.1.1"]])
